d21bd5329e95f2f9b786ba45fe728598bc85df7b
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2022-06-01 03:37:38 +0800

    7th commit

--- lecture/ruang.php
+++ lecture/ruang.php
@@ -54,7 +54,9 @@
 
     <!-- 360deg Ruang -->
     <section class="ruang">
-        <div class="container pt-4">
+        <!-- LANTAI TIGA -->
+        <div id="lantai3" class="container pt-4">
+            <h1>Lantai 3</h1>
             <!-- C302 -->
             <div id="C302">
                 <div class="row">
@@ -242,6 +244,388 @@
                         <h3>C308</h3>
                         <div class="360deg-img">
                             <iframe src="../content/Gambar Ruangan/Lantai 3 Gedung Kuliah/ruang-c308.jpg" frameborder="0" width="100%" style="@media (min-width: 992px) {padding-left: 200px;}"></iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h4>Info:</h4>
+                        <ul>
+                            <li> Kursi 50</li>
+                            <li> Meja 50</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col pb-5">
+                        <div class="table-responsive text-nowrap">
+                            <table class="table table-striped table-hover">
+                                    <?php
+                                        $sql = "SELECT * FROM r401";
+                                        include_once 'dbh.inc.php';
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- C309 -->
+            <div id="C309">
+                <div class="row">
+                    <div class="col">
+                        <h3>C309 - Storage</h3>
+                        <div class="360deg-img">
+                            <iframe src="../content/Gambar Ruangan/Lantai 3 Gedung Kuliah/storage-c309.jpg" frameborder="0" width="100%" style="@media (min-width: 992px) {padding-left: 200px;}"></iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h4>Info:</h4>
+                        <ul>
+                            <li> Kursi 50</li>
+                            <li> Meja 50</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col pb-5">
+                        <div class="table-responsive text-nowrap">
+                            <table class="table table-striped table-hover">
+                                    <?php
+                                        $sql = "SELECT * FROM r401";
+                                        include_once 'dbh.inc.php';
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- C310 -->
+            <div id="C310">
+                <div class="row">
+                    <div class="col">
+                        <h3>C310 - Pantry</h3>
+                        <div class="360deg-img">
+                            <iframe src="../content/Gambar Ruangan/Lantai 3 Gedung Kuliah/pantry-c310.jpg" frameborder="0" width="100%" style="@media (min-width: 992px) {padding-left: 200px;}"></iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h4>Info:</h4>
+                        <ul>
+                            <li> Kursi 50</li>
+                            <li> Meja 50</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col pb-5">
+                        <div class="table-responsive text-nowrap">
+                            <table class="table table-striped table-hover">
+                                    <?php
+                                        $sql = "SELECT * FROM r401";
+                                        include_once 'dbh.inc.php';
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- LANTAI EMPAT -->
+        <div id="lantai4" class="container pt-4">
+            <h1>Lantai 4</h1>
+            <!-- C402 -->
+            <div id="C402">
+                <div class="row">
+                    <div class='col'>
+                        <h3>C402</h3>
+                        
+                        <!-- style="margin:0px; padding:0px; overflow: hidden;" -->
+                        <div class="360deg-img">
+                            <iframe src="../content/Gambar Ruangan/Lantai 4 Gedung Kuliah/ruang-c402.jpg" frameborder="0" width="100%"></iframe>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h4>Info:</h4>
+                        <ul>
+                            <li> Kursi 50</li>
+                            <li> Meja 50</li>
+                        </ul>
+                        <h4>Desc:</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla distinctio neque consequuntur enim atque, voluptatibus suscipit commodi alias eos asperiores.
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col pb-5">
+                        <div class="table-responsive text-nowrap">
+                            <table class="table table-striped table-hover">
+                                    <?php
+                                        $sql = "SELECT * FROM c302";
+                                        include_once 'dbh.inc.php';
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- C403 -->
+            <div id="C403">
+                <div class="row">
+                    <div class='col'>
+                        <h3>C403 - Ruang Kelas</h3>
+                        
+                        <div class="360deg-img">
+                            <iframe src="../content/Gambar Ruangan/Lantai 4 Gedung Kuliah/ruang-c403.jpg" frameborder="0" width="100%"></iframe>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h4>Info:</h4>
+                        <ul>
+                            <li> Kursi 50</li>
+                            <li> Meja 50</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col pb-5">
+                        <div class="table-responsive text-nowrap">
+                            <table class="table table-striped table-hover">
+                                    <?php
+                                        $sql = "SELECT * FROM r401";
+                                        include_once 'dbh.inc.php';
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- C404 -->
+            <div id="C404">
+                <div class="row">
+                    <div class='col'>
+                        <h3>C404</h3>
+                        
+                        <div class="360deg-img">
+                            <iframe src="../content/Gambar Ruangan/Lantai 4 Gedung Kuliah/ruang-c404.jpg" frameborder="0" width="100%"></iframe>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h4>Info:</h4>
+                        <ul>
+                            <li> Kursi 50</li>
+                            <li> Meja 50</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col pb-5">
+                        <div class="table-responsive text-nowrap">
+                            <table class="table table-striped table-hover">
+                                    <?php
+                                        $sql = "SELECT * FROM r401";
+                                        include_once 'dbh.inc.php';
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- C406 -->
+            <div id="C406">
+                <div class="row">
+                    <div class='col'>
+                        <h3>C406</h3>
+                        
+                        <div class="360deg-img">
+                            <iframe src="../content/Gambar Ruangan/Lantai 4 Gedung Kuliah/ruang-c406.jpg" frameborder="0" width="100%"></iframe>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h4>Info:</h4>
+                        <ul>
+                            <li> Kursi 50</li>
+                            <li> Meja 50</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col pb-5">
+                        <div class="table-responsive text-nowrap">
+                            <table class="table table-striped table-hover">
+                                    <?php
+                                        $sql = "SELECT * FROM r401";
+                                        include_once 'dbh.inc.php';
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- C407 -->
+            <div id="C407">
+                <div class="row">
+                    <div class='col'>
+                        <h3>C407</h3>
+                        
+                        <div class="360deg-img">
+                            <iframe src="../content/Gambar Ruangan/Lantai 4 Gedung Kuliah/ruang-c407.jpg" frameborder="0" width="100%"></iframe>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h4>Info:</h4>
+                        <ul>
+                            <li> Kursi 50</li>
+                            <li> Meja 50</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col pb-5">
+                        <div class="table-responsive text-nowrap">
+                            <table class="table table-striped table-hover">
+                                    <?php
+                                        $sql = "SELECT * FROM r401";
+                                        include_once 'dbh.inc.php';
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- C408 -->
+            <div id="C408">
+                <div class="row">
+                    <div class="col">
+                        <h3>C408</h3>
+                        <div class="360deg-img">
+                            <iframe src="../content/Gambar Ruangan/Lantai 4 Gedung Kuliah/ruang-c408.jpg" frameborder="0" width="100%" style="@media (min-width: 992px) {padding-left: 200px;}"></iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h4>Info:</h4>
+                        <ul>
+                            <li> Kursi 50</li>
+                            <li> Meja 50</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col pb-5">
+                        <div class="table-responsive text-nowrap">
+                            <table class="table table-striped table-hover">
+                                    <?php
+                                        $sql = "SELECT * FROM r401";
+                                        include_once 'dbh.inc.php';
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- C409 -->
+            <div id="C409">
+                <div class="row">
+                    <div class="col">
+                        <h3>C409 - Storage</h3>
+                        <div class="360deg-img">
+                            <iframe src="../content/Gambar Ruangan/Lantai 4 Gedung Kuliah/storage-c409.jpg" frameborder="0" width="100%" style="@media (min-width: 992px) {padding-left: 200px;}"></iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h4>Info:</h4>
+                        <ul>
+                            <li> Kursi 50</li>
+                            <li> Meja 50</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col pb-5">
+                        <div class="table-responsive text-nowrap">
+                            <table class="table table-striped table-hover">
+                                    <?php
+                                        $sql = "SELECT * FROM r401";
+                                        include_once 'dbh.inc.php';
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- C410 -->
+            <div id="C410">
+                <div class="row">
+                    <div class="col">
+                        <h3>C410 - Pantry</h3>
+                        <div class="360deg-img">
+                            <iframe src="../content/Gambar Ruangan/Lantai 4 Gedung Kuliah/pantry-c410.jpg" frameborder="0" width="100%" style="@media (min-width: 992px) {padding-left: 200px;}"></iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h4>Info:</h4>
+                        <ul>
+                            <li> Kursi 50</li>
+                            <li> Meja 50</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col pb-5">
+                        <div class="table-responsive text-nowrap">
+                            <table class="table table-striped table-hover">
+                                    <?php
+                                        $sql = "SELECT * FROM r401";
+                                        include_once 'dbh.inc.php';
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Toilet -->
+            <div id="toilet">
+                <div class="row">
+                    <div class="col">
+                        <h3>Toilet</h3>
+                        <div class="360deg-img">
+                            <iframe src="../content/Gambar Ruangan/Lantai 4 Gedung Kuliah/toilet-lp.jpg" frameborder="0" width="100%" style="@media (min-width: 992px) {padding-left: 200px;}"></iframe>
                         </div>
                     </div>
                 </div>
